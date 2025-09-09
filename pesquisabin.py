5daaa680cca9b6b5c26eeb157b48c601c87b89ed
Leonardo Grando <leonardo.grando@acad.unasp.edu.br>
2025-09-09 15:38:57 -0300

    Create pesquisabin.py

---
+++ pesquisabin.py
@@ -0,0 +1,14 @@
+def pesquisa_binaria(lista, item):
+  baixo = 0
+  alto = len(lista) - 1
+
+  while baixo <= alto:
+    meio = (baixo + alto) // 2
+    chute = lista[meio]
+    if chute == item:
+      return meio
+    elif chute > item:
+      alto = meio - 1
+    else:
+      baixo = meio + 1
+  return None
