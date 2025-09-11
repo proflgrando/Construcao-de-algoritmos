389b4494d09bf3cb33c395b7962f801ae549c75d
Leonardo Grando <leonardo.grando@acad.unasp.edu.br>
2025-09-10 22:18:35 -0300

    Update ordenacaoselecao.py

--- ordenacaoselecao.py
+++ ordenacaoselecao.py
@@ -1,17 +1,17 @@
 def buscaMenor(arr):
-  menor = arr[0]
-  menor_indice = 0
-  for i in range(1, len(arr)):
-    if arr[i] < menor:
-      menor = arr[i]
-      menor_indice = i
-  return menor_indice
+    menor = arr[0]
+    menor_indice = 0
+    for i in range(1, len(arr)):
+        if arr[i] < menor:
+            menor = arr[i]
+            menor_indice = i
+    return menor_indice
 
 def ordenacaoporSelecao(arr):
-  novoArr = []
-  for i in range(len(arr)):
-    menor = buscaMenor(arr)
-    novoArr.append(arr.pop(menor))
-  return novoArr
+    novoArr = []
+    for i in range(len(arr)):
+        menor = buscaMenor(arr)
+        novoArr.append(arr.pop(menor))
+    return novoArr
 
 print(ordenacaoporSelecao([10, 5, 2, 3]))
